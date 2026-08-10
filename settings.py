f9ead4e04ac81b6a68923134f6da5f0329c960fd
Vismaya <vismayava6@gmail.com>
2026-08-10 15:31:46 +0530

    fopurt  commit

--- settings.py
+++ settings.py
@@ -173,18 +173,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-# STATIC_URL = 'static/'
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-# ]
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 BASE_DIR = Path(__file__).resolve().parent  # if using Path
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+# MEDIA_URL = "/media/"
+# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 # Default primary key field type
